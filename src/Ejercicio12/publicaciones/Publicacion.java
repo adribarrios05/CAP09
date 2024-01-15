51e3dad7c505dd9bb18ff28e9b217bbb4087e7ce
Adrián <abarmar0501@g.educaand.es>
2024-01-15 20:44:24 +0100

    15/01/2024

---
+++ src/Ejercicio12/publicaciones/Publicacion.java
@@ -0,0 +1,32 @@
+package Ejercicio12.publicaciones;
+
+public class Publicacion {
+    protected String ISBN;
+    protected String titulo;
+    protected int anio;
+
+    public Publicacion(String ISBN, String titulo, int anio){
+        this.ISBN = ISBN;
+        this.titulo = titulo;
+        this.anio = anio;
+    }
+
+    public String getISBN(){
+        return ISBN;
+    }
+
+    public int getAnio(){
+        return anio;
+    }
+
+    public String gettitulo(){
+        return titulo;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("ISBN: %s, título: %s, año de publicación: 1995", this.ISBN, this.titulo, this.anio);
+    }
+
+    
+}
---
+++ src/Ejercicio8/terminales/Terminal.java
@@ -0,0 +1,30 @@
+package Ejercicio8.terminales;
+
+public class Terminal {
+    private int timepoConversacion;
+    private String numTelef;
+    public Terminal(String telef){
+        this.numTelef = telef;
+        this.timepoConversacion = 0;
+    }
+
+    public int getTimepoConversacion() {
+        return timepoConversacion;
+    }
+
+    public String getNumTelef() {
+        return numTelef;
+    }
+
+    public void llama(Terminal t, int segundos){
+        this.timepoConversacion+=segundos;
+        t.timepoConversacion+=segundos;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nº %s - %ss de conversación",
+                this.numTelef,
+                this.timepoConversacion);
+    }
+}
